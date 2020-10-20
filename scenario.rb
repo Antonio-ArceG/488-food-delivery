54edd5462820791c4d0a7205ee19794e0cbfad1e
Caio Andrade <andrade@hey.com>
2020-10-19 19:52:19 -0500

    Delete scenario.rb

--- scenario.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'repositories/patient_repository'
-require_relative 'repositories/room_repository'
-
-room_repo = RoomRepository.new("data/rooms.csv")
-patient_repo = PatientRepository.new("data/patients.csv", room_repo)
-
-
-p patient_repo
